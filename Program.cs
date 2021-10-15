56493d59ebfd4d0be68fa0ab9decb9c6e1761300
MTR\muzykantov.ma <muzykantov.ma@medotrade.ru>
2021-10-15 17:33:54 +0300

    t

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Abstract;
 
 namespace test
 {
@@ -8,27 +9,84 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
-            Console.ReadKey(true);
+            Controller<string> = new Controller<string>().Start();
         }
     }
 
-    interface IController
+    
+
+    public class Controller<T>: IController
     {
-        public void Init();
-        public void Start();
+        private List<T> data;
+        private Viewer viewer;
+        void Init()
+        {
+            data = new List<T>();
+            viewer = new Viewer();
+        }
+
+        public void Start()
+        {
+            if (data == null) Init();
+            View("Init");
+        }
+
+        private void View(string comand)
+        {
+            switch(comand){
+                case "Init": 
+                    viewer.MainMenu();
+                    break;
+            }
+        }
+
+        public Command(string command)
+        {
+            switch(command){
+                case "MainMenu": 
+                    MainMenuHand(Console.ReadLine());
+                    break;
+            }
+        }
+
+        private void MainMenuHand(string Command)
+        {
+            switch(command){
+                case "1": 
+                    Console.WriteLine("вы выбрали пункт 1");
+                    break;
+            }
+        }
     }
 
-    public class Controller: IController
+    public class Viewer
     {
-        //private 
-        public void Init()
+        private Controller controller;
+        public void Init(Controller controller)
+        {
+            this.controller = controller;
+            Console.WriteLine("Здравствуйте! Выберите команду из предложенных");
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("1. Добавить сотрудника, 2. Вывести всех сотрудников");
+            Console.WriteLine("e - выход из программы");
+            controller.Command("MainMenu");
+        }
+        public void ComandHand()
         {
             
+
         }
+    }
+}
+
+namespace Abstract
+{
+    public interface IController
+    {
+        void Init();
+        void Start();
     }
 }
