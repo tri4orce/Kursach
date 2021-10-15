63622a614d676afc205fac3711f1b538e9dc16f2
MTR\muzykantov.ma <muzykantov.ma@medotrade.ru>
2021-10-15 16:14:45 +0300

    initial project

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+
+    interface IController
+    {
+        public void Init();
+        public void Start();
+    }
+
+    public class Controller: IController
+    {
+        private 
+        public void Init()
+        {
+            
+        }
+    }
+}
